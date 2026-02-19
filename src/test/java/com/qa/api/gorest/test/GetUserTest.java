db56b8c203e00b95099d897ea2493b5f32d159de
MallammaHR <mallumanish17@gmail.com>
2026-02-19 09:46:40 -0600

    updated pom.xml

--- src/test/java/com/qa/api/gorest/test/GetUserTest.java
+++ src/test/java/com/qa/api/gorest/test/GetUserTest.java
@@ -4,11 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.aventstack.chaintest.plugins.ChainTestListener;
 import com.qa.api.base.BaseTest;
 import com.qa.api.constants.AuthType;
+import com.qa.api.manager.ConfigManager;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
@@ -23,13 +25,19 @@ import io.restassured.response.Response;
 @Story("US 100 : feature go rest api -  get user api")
 public class GetUserTest extends BaseTest{
 	
+	private String tokenID;
+	@BeforeClass
+	public void setUpToken() {
+		 tokenID = "ef3a7ddaa9f69852a071447d94d8bad5a14350fe1f7221875b69688012982160";
+		ConfigManager.set("bearertoken",tokenID);
+	}
 	@Description("getting all the users......")
 	@Owner("Mallamma Reddy")
 	@Severity(SeverityLevel.CRITICAL)
 	@Test
 	public void getAllUsersTest()
 	{
-		  ChainTestListener.log("Get- all users api Test");
+		 ChainTestListener.log("Get- all users api Test");
 		Response response = restClient.
 		get(BASE_URL_GOREST, GOREST_USERS_ENDPOINT, null, null, AuthType.BEARER_TOKEN, ContentType.JSON);
 		Assert.assertTrue(response.statusLine().contains("OK"));
